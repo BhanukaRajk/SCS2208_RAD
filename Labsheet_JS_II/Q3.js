622235f5ff59d6bf704571f315ca0e16796080ce
BhanukaRajk <bhanukayar@gmail.com>
2022-07-24 17:07:42 +0530

    Q3 completed

--- Labsheet_JS_II/Q3.js
+++ Labsheet_JS_II/Q3.js
@@ -1,7 +1,21 @@
-function Alert() {
+function AlertAGE() {
     let Name = document.getElementById("name").value;
     let BDay = document.getElementById("bday").value;
 
     if (Name != "" && BDay != "") {
-        alert("Birth day: " + BDay); }
-  }
+        const cdate = new Date();
+        const bdate = new Date(document.getElementById("bday").valueAsDate);
+        let CY = cdate.getFullYear();
+        let CM = cdate.getMonth();
+
+        let BY = bdate.getFullYear();
+        let BM = bdate.getMonth();
+
+        if (CM > BM) {
+            alert("Age: " + (CY - BY) + " years and " + (CM - BM) + " months.");
+        }
+        else {
+            alert("Age: " + (CY - BY - 1) + " years and " + (12 + CM - BM) + " months.");
+        }
+    }
+}
